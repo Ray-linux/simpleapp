3b272d19a16cfcb20b85c6a4020d7d1400b55dea
Ray-linux <ranirava52@gmail.com>
2024-05-25 22:59:08 +0530

    test 3

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-import express from "express";
-const app = express();
+import express from 'express';
 
+const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res)=>{
-    res.json("Hello vercel");
-})
 
-app.listen(PORT, ()=>{
-    console.log("running")
-})
+/* routes */
+
+// app.use('/api', router)
+
+app.get('/', (req, res)=>{
+    try{
+        res.send("Hello vercel")
+    }catch(e){
+        res.json(e)
+    }
+});
+
+try{
+    app.listen(PORT, ()=>{
+        console.log(`server is running in http://localhost:${PORT}`);
+    })
+}catch(e){
+    console.log(e)
+}
